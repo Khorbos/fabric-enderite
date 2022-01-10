f48a792c91b3c0cdad68427ab42eea31088cd9a7
Khorbos <korbos.pro@gmail.com>
2022-01-10 19:49:18 +0100

    feat(item group): add item group.
    There is no real ItemStack for the moment, waiting to the add of items for this.

--- src/main/java/com/khorbos/enderite/Enderite.java
+++ src/main/java/com/khorbos/enderite/Enderite.java
@@ -3,10 +3,15 @@ package com.khorbos.enderite;
 import com.khorbos.enderite.init.BlockRegistries;
 import com.khorbos.enderite.init.ItemRegistries;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
 public class Enderite implements ModInitializer {
 	public static final String ID = "enderite";
 	public static final String NAME = "Enderite Mod";
+	public static final ItemGroup ENDERITE_GROUP = FabricItemGroupBuilder.build(new Identifier(ID, "enderite"), () -> new ItemStack(null));
 
 	@Override
 	public void onInitialize() {
